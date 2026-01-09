3bd1e73937b73f828698d7abdab74c66c401f4b1
Dipesh <dipeshluitel40@gmail.com>
2026-01-09 14:20:19 +0545

    add(models): 3 models reqired for project added and migrated

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    live_site = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.ImageField(upload_to='skills/')
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
