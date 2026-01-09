395d61c28ae7af6c5149f029c08300ef00da1be3
Dipesh <dipeshluitel40@gmail.com>
2026-01-09 14:51:22 +0545

    add(views): home view added and tested after rounting including project .urls

--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Project,Skill
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+    return HttpResponse("Hello Home")
+    # return render(request, 'portfolio/home.html', {'projects': projects, 'skills': skills})
