4e344b3af9a23727924c2eea979b1e849d6d0a3c
Dipesh <dipeshluitel40@gmail.com>
2026-01-10 22:58:41 +0545

    update(models): models.py updated to track skill status

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+SKILL_STATUS = (
+    ('y','learned'),
+    ('n','learning')
+)
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -13,6 +18,13 @@ class Project(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=100)
     icon = models.ImageField(upload_to='skills/')
+    status = models.CharField(max_length=1,choices=SKILL_STATUS,null=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
