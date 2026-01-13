9dbb3784a578fb28b2e4761593717043b563ac45
Dipesh <dipeshluitel40@gmail.com>
2026-01-13 20:44:30 +0545

    update(templates): display for form has been improved

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -31,7 +31,7 @@ class Skill(models.Model):
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone_number = PhoneNumberField(null=True,blank=True,verbose_name="Phone Number")
+    phone_number = PhoneNumberField(null=True,blank=True,verbose_name="Phone Number",region='NP',unique=True)
 
     message = models.TextField()
 
