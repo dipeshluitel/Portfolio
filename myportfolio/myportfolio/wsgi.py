23247c679f99e4d3f2f59ba91fb0988344f77286
Dipesh <dipeshluitel40@gmail.com>
2026-01-16 18:25:57 +0545

    updating WSGI for testing temporarily

--- myportfolio/myportfolio/wsgi.py
+++ myportfolio/myportfolio/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+def application(environ, start_response):
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return [b"WSGI OK"]
+
