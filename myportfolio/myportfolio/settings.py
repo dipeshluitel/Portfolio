364b843a99b7d8ecb19970d0b09d898f66b36bfc
Dipesh <dipeshluitel40@gmail.com>
2026-01-17 15:43:54 +0545

    allowed host configured again

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--hr8zl$*+l)+e4rv=l9^e*c^l6978_mi4d_k&9o5qkzdlk!esr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 # ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ["https://portfolio-production-030c.up.railway.app","https://127.0.0.1:8000"]
 
