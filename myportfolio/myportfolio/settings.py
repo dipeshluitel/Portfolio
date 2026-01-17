6447d43b2c251f6bdb2470740b3ae5c72d04db78
Dipesh <dipeshluitel40@gmail.com>
2026-01-17 21:59:53 +0545

    testing for DEBUG=

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -33,7 +33,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--hr8zl$*+l)+e4rv=l9^e*c^l6978_mi4d_k&9o5qkzdlk!esr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+# DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = True
 
 
 
